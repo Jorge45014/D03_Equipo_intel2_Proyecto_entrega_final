344f163c8a16d31e3131b7d2e8c12b3fe6db5032
Jorge45014 <jorge.sanchez4716@alumnos.udg.mx>
2024-11-23 16:12:47 -0600

    Add files via upload

---
+++ archivos/Operaciones.asm
@@ -0,0 +1,33 @@
+ADDI $11 $4 #10
+nop
+nop
+nop
+SLTI $12 $4 #49
+nop
+nop
+nop
+ANDI $13 $8 #15
+nop
+nop
+nop
+ORI $14 $8 #15
+nop
+nop
+nop
+SW $5 $9 #2
+nop
+nop
+nop
+LW $10 $9 #2
+nop
+nop
+nop
+BEQ $5 $10 #1
+nop
+nop
+nop
+ADDI $15 $3 #325
+nop
+nop
+nop
+ADDI $16 $2 #74
